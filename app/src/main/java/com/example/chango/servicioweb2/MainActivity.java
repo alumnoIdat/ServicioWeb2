0f0f729605946d94489fe74abdae113b07d9b387
alumnoIdat <leomanarael@gmail.com>
2016-12-02 09:28:42 -0500

    final

--- app/src/main/java/com/example/chango/servicioweb2/MainActivity.java
+++ app/src/main/java/com/example/chango/servicioweb2/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.chango.servicioweb2;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +33,70 @@ public class MainActivity extends AppCompatActivity {
     ListView lista;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        empresaList = new ArrayList<HashMap<String, String>>();
+        new LoadAllProducts().execute();
+        lista = (ListView) findViewById(R.id.listAllProducts);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Cargando comercios. Por favor espere...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... args) {
+            List params = new ArrayList();
+            JSONObject json = jParser.makeHttpRequest(
+                    url_all_empresas, "GET", params);
+            Log.d("All Products: ", json.toString());
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    products=json.getJSONArray(TAG_PRODUCTS);
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NOMBRE);
+                        HashMap map = new HashMap();
+                        map.put(TAG_ID, id);
+                        map.put(TAG_NOMBRE, name);
+                        empresaList.add(map);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String file_url) {
+            pDialog.dismiss();
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    ListAdapter adapter = new SimpleAdapter(
+                            MainActivity.this, empresaList, R.layout.single_post,
+                            new String[]{
+                                    TAG_ID,
+                                    TAG_NOMBRE,
+                            },
+                            new int[]{
+                                    R.id.single_post_tv_id,
+                                    R.id.single_post_tv_nombre,
+                            });
+                    //setListAdapter(adapter);
+                    lista.setAdapter(adapter);
+                }
+            });
+        }
     }
 }
